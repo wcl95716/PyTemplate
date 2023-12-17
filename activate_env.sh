3381958fcae97b2aa4ede049cface142711bc152
panda <347038217@qq.com>
2023-12-18 03:48:40 +0800

    update

--- activate_env.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-conda env list | grep ticketing-website || conda create --name ticketing-website python=3.9
-eval "$(conda shell.bash hook)"
-conda activate ticketing-website
-
-# 你的其他命令
-# 例如：
-# python -m pip install --upgrade pip
-# pip install -r requirements.txt
