c0ce84e7fdf808c6dcf12c4f1cd778d98ee831b5
panda <347038217@qq.com>
2023-12-25 20:17:38 +0800

    update

--- src/api/fastapi.py
+++ src/api/fastapi.py
@@ -18,3 +18,23 @@ fast_api.include_router(
 @fast_api.get("/")
 def read_root() -> dict[str, str]:
     return {"message": "Hello, World!"}
+
+
+
+# @api_bp.route('/msg_cb', methods=['POST'])
+# def message_callback():
+#     if request.method == 'POST':
+#         # 解析JSON数据
+#         data = request.get_json()
+#         # print("data ",data)
+
+#         # 获取消息内容
+#         message_content = data.get('content', '')
+
+#         # 处理消息，这里简单打印消息内容
+#         print("Received message:", message_content)
+        
+#         sendMsg(message_content,"Panda")
+
+#         # 返回成功响应
+#         return jsonify({"status": "success"})
