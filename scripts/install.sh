eed85f43590f87ca6c1371ce21d49e01d5a664e3
panda <347038217@qq.com>
2024-01-17 01:14:28 +0800

    update

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,12 +5,15 @@ curl -fsSL https://get.docker.com -o get-docker.sh && sh get-docker.sh
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
     # Linux操作系统
     echo "当前操作系统是Linux"
-    curl -O https://repo.anaconda.com/archive/Anaconda3-2022.05-Linux-x86_64.sh | bash
-
+    curl -O https://repo.anaconda.com/archive/Anaconda3-2023.09-0-Linux-x86_64.sh && bash Anaconda3-2023.09-0-Linux-x86_64.sh
+    
     # 在这里执行适用于Linux的命令
 elif [[ "$OSTYPE" == "darwin"* ]]; then
     # macOS操作系统
     echo "当前操作系统是macOS"
+    # Anaconda3-2023.09-0-MacOSX-x86_64.sh
+    curl -O https://repo.anaconda.com/archive/Anaconda3-2023.09-0-MacOSX-x86_64.sh && bash Anaconda3-2023.09-0-MacOSX-x86_64.sh
+    # 在这里执行适用于Linux的命令
     # 在这里执行适用于macOS的命令
 elif [[ "$OSTYPE" == "cygwin" || "$OSTYPE" == "msys" || "$OSTYPE" == "win32" ]]; then
     # Windows操作系统
