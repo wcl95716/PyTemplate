ebfd11fbbcf16d201112332260313aec696dfbe8
panda <347038217@qq.com>
2024-01-17 03:32:53 +0800

    update

--- scripts/init_ssl.sh
+++ scripts/init_ssl.sh
@@ -71,7 +71,7 @@ function install_or_renew_letsencrypt_tls() {
 
         # 使用 acme.sh 的完整路径生成证书
         echo "正在从 Let's Encrypt 获取证书..."
-        "$ACME_SH" --issue --standalone -d "$domain" --keylength ec-256 --server letsencrypt
+        "$ACME_SH" --issue --standalone -d "$domain" --keylength ec-256 --server letsencrypt --force
     fi
 
     # 再次检查证书是否生成成功
