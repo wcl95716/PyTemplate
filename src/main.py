f677a731bd549d5ffec2bc608026bd1c8befaef5
panda <347038217@qq.com>
2024-02-05 01:48:20 +0800

    update

--- src/main.py
+++ src/main.py
@@ -7,6 +7,6 @@ from api.fastapi import fast_api
 
 if __name__ == "__main__":
     import uvicorn
-    # uvicorn.run("main:fast_api", host=NetworkUtils.get_ipv6_address(), port=25432, reload=True)
-    uvicorn.run("main:fast_api", host=NetworkUtils.get_ipv4_address(), port=25432, reload=True)
+    uvicorn.run("main:fast_api", host=NetworkUtils.get_ipv6_address(), port=25432, reload=True)
+    # uvicorn.run("main:fast_api", host=NetworkUtils.get_ipv4_address(), port=25432, reload=True)
     
